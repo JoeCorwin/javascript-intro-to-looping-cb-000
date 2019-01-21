dac8768b91e0dab4893fb1057c7465794f6a2d14
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 17:49:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,18 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if i = 1
-      array.push('I am ${i} strange loop.')
-    else
-      array.push('I am ${i} strange loops')
-  }
+    if (i = 1) {
+      array.push(`I am ${i} strange loop.`)
+    }
+    else (){
+      array.push(`I am ${i} strange loops`)
+    }
   return array
 }
+
+// if (lightIsGreen) {
+//   go()
+// } else if (lightIsRed) {
+//   stop()
+// } else if (lightIsYellow) {
+//   slowDown()
+// }
