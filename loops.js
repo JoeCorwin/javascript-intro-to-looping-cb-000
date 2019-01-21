eb7a343e3111061e09e53363d5fff47fddb500dd
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 18:01:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -7,14 +7,17 @@ function forLoop(array) {
     else {
       array.push(`I am ${i} strange loops`)
     }
-  return array
   }
+  return array;
 }
 
-// if (lightIsGreen) {
-//   go()
-// } else if (lightIsRed) {
-//   stop()
-// } else if (lightIsYellow) {
-//   slowDown()
+// function forLoop(array) {
+//    for ( let i = 0; i < 25; i++) {
+//      if (i === 1) {
+//        array.push(`I am 1 strange loop.`)
+//      } else {
+//        array.push(`I am ${i} strange loops.`)
+//      }
+//   }
+//   return array;
 // }
