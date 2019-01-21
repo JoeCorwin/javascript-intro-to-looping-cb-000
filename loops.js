247eee925dfe180da589342745c213b471f51b80
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 18:19:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,12 +11,14 @@ function forLoop(array) {
   return array;
 }
 
+
 function whileLoop(n) {
     while (n > 0) {
       console.log(--n)
     }
     return 'done'
 }
+
 
 function doWhileLoop(n) {
   do {console.log(--n);}
